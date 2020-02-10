b08e175ea1d4528d16a8a3d71703baea788b0895
Roman Chopovenko <rchopovenko@vinditek.com>
2020-02-10 10:11:18 +0200

    Move `RemoteFeedLoader` to production

---
+++ EssentialFeed/Feed API/RemoteFeedLoader.swift
@@ -0,0 +1,27 @@
+//
+//  RemoteFeedLoader.swift
+//  EssentialFeed
+//
+//  Created by Roman Chopovenko on 09.02.2020.
+//  Copyright © 2020 romito. All rights reserved.
+//
+
+import Foundation
+
+public protocol HTTPClient {
+    func get(from url: URL?)
+}
+
+public final class RemoteFeedLoader {
+    private let url: URL
+    private let client: HTTPClient
+    
+    public init(url: URL, client: HTTPClient) {
+        self.client = client
+        self.url = url
+    }
+    
+    public func load() {
+        client.get(from: url)
+    }
+}
